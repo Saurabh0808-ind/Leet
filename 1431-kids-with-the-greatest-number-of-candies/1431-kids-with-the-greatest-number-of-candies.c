9035ca0b4d34420c9bbaf3989d855a13bd48c3c6
Saurabh K <www.saurabhsanjay@gmail.com>
2026-09-24 01:04:39 +0530

    Time: 0 ms (100%), Space: 10.5 MB (6.03%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.c
@@ -0,0 +1,19 @@
+bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
+    bool* result = (bool*)malloc(candiesSize * sizeof(bool));
+
+    *returnSize = candiesSize;
+
+    int max = candies[0];
+
+    for (int i = 1; i < candiesSize; i++) {
+        if (candies[i] > max) {
+            max = candies[i];
+        }
+    }
+
+    for (int i = 0; i < candiesSize; i++) {
+        result[i] = candies[i] + extraCandies >= max;
+    }
+
+    return result;
+}
