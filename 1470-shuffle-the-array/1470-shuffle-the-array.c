56624fe383f207da7e6d4b7fc61a0bd27b1dbc5d
Saurabh K <www.saurabhsanjay@gmail.com>
2026-09-23 23:50:44 +0530

    Time: 13 ms (51.43%), Space: 12.1 MB (99.27%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.c
@@ -0,0 +1,14 @@
+int* shuffle(int* nums, int numsSize, int n, int* returnSize) {
+    int* result = (int*)malloc(numsSize * sizeof(int));
+
+    *returnSize = numsSize;
+
+    int j = 0;
+
+    for (int i = 0; i < n; i++) {
+        result[j++] = nums[i];
+        result[j++] = nums[i + n];
+    }
+
+    return result;
+}
