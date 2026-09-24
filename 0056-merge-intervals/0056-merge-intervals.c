63dd4e1568718e33f5c3d758d4f33e84ee292e48
Saurabh K <www.saurabhsanjay@gmail.com>
2026-09-24 17:48:20 +0530

    Time: 3 ms (62.47%), Space: 18.8 MB (47.41%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+    int* x = *(int**)a;
+    int* y = *(int**)b;
+
+    return x[0] - y[0];
+}
+
+int** merge(int** intervals, int intervalsSize, int* intervalsColSize,
+            int* returnSize, int** returnColumnSizes) {
+
+    qsort(intervals, intervalsSize, sizeof(int*), compare);
+
+    int** result = malloc(intervalsSize * sizeof(int*));
+    *returnColumnSizes = malloc(intervalsSize * sizeof(int));
+
+    int count = 0;
+
+    for (int i = 0; i < intervalsSize; i++) {
+        if (count == 0 || intervals[i][0] > result[count - 1][1]) {
+            result[count] = malloc(2 * sizeof(int));
+            result[count][0] = intervals[i][0];
+            result[count][1] = intervals[i][1];
+            (*returnColumnSizes)[count] = 2;
+            count++;
+        } else {
+            if (intervals[i][1] > result[count - 1][1])
+                result[count - 1][1] = intervals[i][1];
+        }
+    }
+
+    *returnSize = count;
+    return result;
+}
